37d0673b2ab6e99605afbda23ca85fa06467d315
ilona1009 <maliarenko.ilona@gmail.com>
2023-03-28 11:17:57 +0200

    add commit one

---
+++ src/js/02-video.js
@@ -0,0 +1,13 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+ const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
