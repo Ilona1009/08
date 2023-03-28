cea400f122d3088c48de023454bbee324193ea29
ilona1009 <maliarenko.ilona@gmail.com>
2023-03-28 16:44:33 +0200

    two

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,12 +2,12 @@ import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
  const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+    
+const onPlay = function (data) {
+    const localCurrentTime = localStorage.setItem("videoplayer-current-time",data.seconds)
+}
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('timeupdate', throttle(onPlay, 1000));
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+    player.setCurrentTime(localStorage.getItem('localCurrentTime') || 0)
