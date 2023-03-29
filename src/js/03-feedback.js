d8269f657f950d4da5eba3a4aa1bba0bd18277be
ilona1009 <maliarenko.ilona@gmail.com>
2023-03-29 11:49:49 +0200

    three

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('[name=email');
+const messageRef = document.querySelector('[name=message');
+
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', onFormInput);
+// emailRef.addEventListener('input', onEmailInput);
+
+function onFormInput(e) {
+    const dataForm = {
+        email: emailRef.value,
+        messageRef: messageRef.value,
+    }
+            console.log(dataForm);
+
+}
+
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state')
+
+}
+
+
+
+
+
 
