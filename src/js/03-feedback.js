0b0563df1cca70a5eecfe4b627b4f395923557d9
ilona1009 <maliarenko.ilona@gmail.com>
2023-03-30 11:49:08 +0200

    four

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,38 @@
 import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
-const emailRef = document.querySelector('[name=email');
-const messageRef = document.querySelector('[name=message');
+const emailRef = document.querySelector('[name=email]');
+const messageRef = document.querySelector('[name=message]');
 
 
+formRef.addEventListener('input', throttle(onFormInput, 500));
 formRef.addEventListener('submit', onFormSubmit);
-formRef.addEventListener('input', onFormInput);
-// emailRef.addEventListener('input', onEmailInput);
 
-function onFormInput(e) {
+
+function onFormInput() {
     const dataForm = {
         email: emailRef.value,
-        messageRef: messageRef.value,
+        message: messageRef.value,
     }
-            console.log(dataForm);
-
+        localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
 }
+
+function localStorageSaveData (){
+    let savedData = JSON.parse(localStorage.getItem('feedback-form-state'))
+     if (savedData) {
+         emailRef.value = savedData.email; 
+         messageRef.value = savedData.message;
+    }
+}
+
+localStorageSaveData();
 
 
 function onFormSubmit(e) {
     e.preventDefault();
 
     e.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state')
-
+    localStorage.removeItem('feedback-form-state');
 }
 
 
